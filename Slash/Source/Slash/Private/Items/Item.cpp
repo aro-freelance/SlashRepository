dc30be1e3b1d6e2af93572be83e999c6546f3a2c
aro-freelance <aro.freelance@gmail.com>
2023-07-13 13:00:45 -0400

    5.42
    Item Actor name and id to screen

--- Slash/Source/Slash/Private/Items/Item.cpp
+++ Slash/Source/Slash/Private/Items/Item.cpp
@@ -17,12 +17,34 @@ void AItem::BeginPlay()
 
 	UE_LOG(LogTemp, Warning, TEXT("BeginPlay called."));
 
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(1000, 10.f, FColor::Cyan, FString("Item Message on Screen"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("GEngine is null."));
+	}
+
 	
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
 
+	if (GEngine) {
+
+		FString Name = GetName(); 
+
+		int index = GetUniqueID();
+
+		FString Message = FString::Printf(TEXT("Item name: %s"), *Name);
+
+		GEngine->AddOnScreenDebugMessage(index, 10.f, FColor::Cyan, Message);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("GEngine is null."));
+	}
+	
 }
 
