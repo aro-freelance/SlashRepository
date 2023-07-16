b3c5ba3108e665c2b225facb166e0e6ea3eb9e89
aro-freelance <aro.freelance@gmail.com>
2023-07-16 11:58:26 -0400

    6.60
    Template Functions, Setting Components with C++, Actor Rotation

--- Slash/Source/Slash/Private/Items/Item.cpp
+++ Slash/Source/Slash/Private/Items/Item.cpp
@@ -12,16 +12,15 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
+
 }
 
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
-
-	
-	
 }
 
 float AItem::TransformedSin()
@@ -40,39 +39,8 @@ void AItem::Tick(float DeltaTime)
 
 	RunningTime += DeltaTime;
 
-	//float DeltaZ = Amplitude * FMath::Sin(RunningTime * Speed);
-
-	//AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
-
-	//AddActorWorldRotation(FRotator(0.f, RotationRate, 0.f));
-
-	UWorld* World = GetWorld();
-	FVector Location = GetActorLocation();
-	FRotator Rotation = GetActorRotation();
-	FVector ForwardVector = GetActorForwardVector();
-	float Length = 100.f;
-
-	DRAW_SPHERE_SINGLE_FRAME(Location, FColor::Blue);
-	DRAW_VECTOR_SINGLE_FRAME(Location, ForwardVector, Length, FColor::Green);
-
-
-	
-/*
-	if (GEngine) {
-
-		FString Name = GetName(); 
-
-		int index = GetUniqueID();
-
-		FString Message = FString::Printf(TEXT("Item name: %s"), *Name);
-
-		GEngine->AddOnScreenDebugMessage(index, 10.f, FColor::Cyan, Message);
-	}
-	else {
-		UE_LOG(LogTemp, Warning, TEXT("GEngine is null."));
-	} 
-*/
-	
+	AddActorWorldRotation(FRotator(
+		(RotationRateX * DeltaTime), (RotationRateY * DeltaTime), (RotationRateZ * DeltaTime)));
 
 }
 
--- Slash/Source/Slash/Public/Items/Item.h
+++ Slash/Source/Slash/Public/Items/Item.h
@@ -22,16 +22,25 @@ protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trig Parameters")
-		float SinAmplitude = 1.f;
+	float SinAmplitude = 1.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trig Parameters")
-		float SinSpeed = 10.f;
+	float SinSpeed = 10.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trig Parameters")
-		float CosAmplitude = 1.f;
+	float CosAmplitude = 1.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trig Parameters")
-		float CosSpeed = 10.f;
+	float CosSpeed = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	float RotationRateX = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	float RotationRateY = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	float RotationRateZ = 0.f;
 
 	UFUNCTION(BlueprintPure)
 	float TransformedSin();
@@ -39,12 +48,24 @@ protected:
 	UFUNCTION(BlueprintPure)
 	float TransformedCos();
 
+	template<typename T>
+	T Avg(T First, T Second);
+
 
 private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime = 0;
 
-	
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
 
 };
+
+template<typename T>
+inline T AItem::Avg(T First, T Second)
+{
+
+	return (First + Second) / 2;
+}
