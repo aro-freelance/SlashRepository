a8a33073fd3219a957f0fef216a7d5df6314f700
aro-freelance <aro.freelance@gmail.com>
2023-11-26 12:09:30 -0500

    21.211
    Soul Blueprint made based on C++ class which is child of item class, niagara effect improved

--- Slash/Source/Slash/Private/Items/Soul.cpp
+++ Slash/Source/Slash/Private/Items/Soul.cpp
@@ -3,3 +3,10 @@
 
 #include "Items/Soul.h"
 
+void ASoul::OnCollisionCapsuleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnCollisionCapsuleOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+
+
+}
